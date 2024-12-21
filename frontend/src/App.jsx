beaa3cfb104a1b50ebb8ad29fa87819d593887b0
jayakumar <jayakumar05122000@gmail.com>
2024-12-21 16:34:53 +0530

    feat: Update app.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-gradient-to-r from-pink-500 via-blue-400 to-red-500 w-full h-screen flex justify-center items-center'>
-      <h1 className='text-[200px] uppercase text-white'><span className='text-[250px] text-yellow-300 animate-bounce'>Z</span>evagoTrip</h1>
+    <div className='bg-gradient-to-r from-pink-500 via-blue-400 to-red-500 w-full h-screen flex justify-center items-center flex-col'>
+      <h1 className='text-[200px] uppercase text-white'><span className='text-[250px] text-yellow-300 '>Z</span>evagoTrip</h1>
+      <p className='font-semibold text-white text-xl'>Developers : Jk and Vedha</p>
+      <p  className='font-semibold text-white'>Coming soon...</p>
     </div>
   )
 }
