71a4d7bff7cd7bb721a3c84c088ae566a6bda71a
Vedharaj <crisvedharaj@gmail.com>
2024-12-21 14:37:51 +0530

    vite setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-slate-700 w-100'>
+      <h1 className='text-2xl text-center'>ZevagoTrip</h1>
+    </div>
+  )
+}
+
+export default App
