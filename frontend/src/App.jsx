819ceb86c26933fd96998ed94c3b9135d17fadad
vedharaj <crisvedharaj@gmail.com>
2024-12-26 16:58:24 +0530

    setup router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+
+import Home from './pages/Home'
+import ContactUs from './pages/ContactUs'
+import AboutUs from './pages/AboutUs'
+import Destination from './pages/Destination'
+import TravelGuide from './pages/TravelGuide'
 
 const App = () => {
   return (
-    <div className='bg-gradient-to-r from-pink-500 via-blue-400 to-red-500 w-full h-screen flex justify-center items-center flex-col'>
-      <h1 className='text-[200px] uppercase text-white'><span className='text-[250px] text-yellow-300 '>Z</span>evagoTrip</h1>
-      <p className='font-semibold text-white text-xl'>Developers : Jk and Vedha</p>
-      <p  className='font-semibold text-white'>Coming soon...</p>
+    <div className='text-inter'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='contact-us' element={<ContactUs />} />
+        <Route path='about-us' element={<AboutUs />} />
+        <Route path='destination' element={<Destination />} />
+        <Route path='travel-guide' element={<TravelGuide />} />
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/TravelGuide.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TravelGuide = () => {
+  return (
+    <div>TravelGuide</div>
+  )
+}
+
+export default TravelGuide
